b102672517d8fd6e76aca195996457ddb6d8d682
Marco Segreto <marco.segreto@gsa.gov>
2015-04-02 15:04:00 -0700

    Testem setup complete
    Chrome isn't working now because of something odd so will be ignoring

--- test/rum-speedindex_test.js
+++ test/rum-speedindex_test.js
@@ -2,7 +2,7 @@ var test = require('tape');
 
 var SpeedIndex = require('../src/rum-speedindex');
 
-test('hello says hello', function(t){
-  t.plan(1);
-  t.equal(SpeedIndex, 'hello world', 'test it');
+test('it exists', function(t){
+  t.ok(SpeedIndex, 'Speed Index exists');
+  t.end();
 });
