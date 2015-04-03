a11f6f92a322ba9fa2c14f8a169ee19bbcda63db
Marco Segreto <marco.segreto@gsa.gov>
2015-04-02 17:31:04 -0700

    Working on convering various modules
    I like playing around with es6, but think it adds way too much overhead to
    the final file size. May have to ditch it.

--- test/rum-speedindex_test.js
+++ test/rum-speedindex_test.js
@@ -1,8 +1,37 @@
-var test = require('tape');
+import test from 'tape';
+import sinon from 'sinon';
 
-var SpeedIndex = require('../src/rum-speedindex');
+import * as rSI from '../src/rum-speedindex';
 
-test('it exists', function(t){
-  t.ok(SpeedIndex, 'Speed Index exists');
+let testEl = {};
+
+test('it exists', t => {
+  t.ok(rSI, 'Speed Index exists');
+  t.end();
+});
+
+// CheckElement
+test('checkElement exists', t => {
+  t.ok(rSI.CheckElement, 'it exists');
+  t.end();
+});
+
+test('checkElement returns null if no url passed', t => {
+  let actual = rSI.CheckElement({});
+  t.ok(!actual, 'returns null or undefined');
+  t.end();
+});
+
+test('checkElement returns null if GetElementViewportRect returns false', t => {
+  let GetElementViewportRectSource = rSI.__get__('GetElementViewportRect');
+  rSI.__set__('GetElementViewportRect', el => {
+    return false;
+  });
+
+  let actual = rSI.CheckElement(testEl, 'http://url.com');
+  t.ok(!actual, 'returns null or undefined');
+
+  rSI.__set__('GetElementViewportRect', GetElementViewportRectSource);
+
   t.end();
 });
